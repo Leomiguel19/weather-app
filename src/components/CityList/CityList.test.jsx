89ba7c9bab23eee5dd8147c1684c07de3d5847b4
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-06 04:45:58 -0400

    Added test to CityList

--- src/components/CityList/CityList.test.jsx
+++ src/components/CityList/CityList.test.jsx
@@ -1,3 +1,22 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import CityList from './CityList' //SUT
+
+const cities = [
+    { city: "Buenos Aires", country: "Argentina"},
+    { city: "Caracas", country: "Venezuela"},
+    { city: "Bógota", country: "Colombia"},
+    { city: "Ciudad de México", country: "México"},
+    { city: "Santiago", country: "Chile"},
+]
+
+
 test("CityList render", async () => {
-    
+    // AAA Arrange Act Assert
+
+    const { findAllByRole } = render(<CityList cities={cities} />)
+
+    const items = await findAllByRole("listitem") 
+
+    expect(items).toHaveLength(5) 
 })
