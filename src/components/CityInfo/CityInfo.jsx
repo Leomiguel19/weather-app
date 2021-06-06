26fc9a6a5a5810f79af5062688539779c68d5ddd
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-05 20:19:16 -0400

    Added more content to CityInfo Component

--- src/components/CityInfo/CityInfo.jsx
+++ src/components/CityInfo/CityInfo.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const CityInfo = props => {
+const CityInfo = ({ city, country }) => {
     return (
-        <div>
+        <>
             
-        </div>
+        </>
     )
 }
 
 CityInfo.propTypes = {
-
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired
 }
 
 export default CityInfo
