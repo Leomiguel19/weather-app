ff2a52cf65ad71de35011578d178a60d47ed7e37
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-06 03:16:53 -0400

    Weather component creation with test and stories

---
+++ src/components/Weather/Weater.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import Weather from './Weather'
+import '@testing-library/jest-dom/extend-expect'
+
+test("Weather render", async () => {
+    // AAA Arrange --> Act --> Assert
+    const { findByRole } = render(<Weather temperature={10}/>)
+
+    const temp = await findByRole("heading")
+
+    expect(temp).toHaveTextContent("10")
+})
