2158be62260674eb3ef8130b2c847c28d1b01984
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-07 02:25:13 -0400

    Added PropOneOf validation to Weather

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -8,6 +8,14 @@ import { WiCloud,
     WiRain } from 'react-icons/wi'
 import { IconContext } from 'react-icons'
 
+const validValues = [
+    "cloud",
+    "cloudy",
+    "fog",
+    "sunny",
+    "rain",
+]
+
 const stateByName = {
     cloud: WiCloud,
     cloudy: WiDayCloudy,
@@ -38,7 +46,7 @@ const Weather = ({temperature, state}) => {
 
 Weather.propTypes = {
     temperature: PropTypes.number.isRequired,
-    state: PropTypes.string.isRequired
+    state: PropTypes.oneOf(validValues).isRequired
 }
 
 export default Weather
