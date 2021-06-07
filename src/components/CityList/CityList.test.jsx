b3c9755aae5a880029382a5bc825c0bd980b9555
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-07 02:40:56 -0400

    Added event to CityList Using TDD

--- src/components/CityList/CityList.test.jsx
+++ src/components/CityList/CityList.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import CityList from './CityList' //SUT
 
 const cities = [
@@ -19,4 +19,24 @@ test("CityList render", async () => {
     const items = await findAllByRole("listitem") 
 
     expect(items).toHaveLength(5) 
+})
+
+test("CityList click o item", async () => {
+    // Debemos simular una acción del usuario: click sobre un item
+    // Para eso vamos a utilizar una función "mock"
+    const fnClickOnItem = jest.fn()
+
+    const { findAllByRole } = render(<CityList cities={cities} onClickCity={fnclickOnItem} />)
+
+    const items = await findAllByRole("listitem")
+
+    // Ahora, para simular la acción, vamos a utilizar fireEvent
+    // fireEvent es parte de la librería testing-library/react
+
+    fireEvent.click(items[0])
+
+    // ¿Ahora que tuvo que suceder?
+    // Se debio llamar a la función fnClickOnItem UNA única vez
+
+    expect(fnClickOnItem).toHaveBeenCalledTimes(1)
 })
