e36226e0efd489a977c9cf67a66bbeff5d028edd
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-07 01:28:54 -0400

    AddOn to Storybook installation for ViewPort

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -13,13 +13,15 @@ const renderCityAndCountry = cityAndCountry => {
             <Grid container
                 justify="center"
                 alignItems="center"
-            >
+            >                 
                 <Grid item
-                    sm={8}>
+                    md={4}
+                    sm={12}>
                     <CityInfo city={city} country={country} />                    
                 </Grid>
                 <Grid item
-                sm={4}>
+                md={4}
+                sm={12}>
                     <Weather temperature={10} state="sunny"/>
                 </Grid>
             </Grid>        
