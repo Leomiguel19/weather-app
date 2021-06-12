f90522a9a407b025fc19d8a1e071b8b0d468ed48
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-12 00:05:01 -0400

    Added ForecastItem component

---
+++ src/components/ForecastItem/ForecastItem.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+
+const ForecastItem = ({ weekDay, hour, state, temperature }) => {
+    return (
+        <Grid container
+            direction="column"
+            justify="center"
+            alignItems="center"
+        >
+            <Grid item>
+                <Typography>{weekDay}</Typography>
+            </Grid>
+
+            <Grid item>
+                <Typography>{hour}</Typography>
+            </Grid>
+
+            <Grid item>
+                [icono]{state}
+            </Grid>
+
+            <Grid item>
+                <Typography>{temperature} º</Typography>
+            </Grid>            
+        </Grid>
+    )
+}
+
+ForecastItem.propTypes = {
+    weekDay: PropTypes.string.isRequired,
+    hour: PropTypes.number.isRequired,
+    state: PropTypes.number.isRequired,
+    temperature: PropTypes.number.isRequired,
+}
+
+export default ForecastItem
