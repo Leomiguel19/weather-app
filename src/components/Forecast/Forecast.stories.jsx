7a0d003915d6abbac6235cc690832b321ae7c0c7
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-12 03:37:06 -0400

    Added stories to Forecast

---
+++ src/components/Forecast/Forecast.stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Forecast from './Forecast'
+
+export default {
+    title: "Forecast",
+    component: Forecast
+}
+
+const forecastItemList = [
+    { hour: 18, state:"sunny", temperature:17, weekDay:"Jueves" },
+    { hour: 12, state:"cloud", temperature:18, weekDay:"Viernes"},
+    { hour: 6, state:"fog", temperature:18, weekDay:"Viernes"},
+    { hour: 14, state:"cloudy", temperature:19, weekDay:"Viernes"},
+    { hour: 15, state:"rain", temperature:17, weekDay:"Sábado"},
+    { hour: 9, state:"rain", temperature:17, weekDay:"Sábado"},
+]
+export const ForecastExample = () => (<Forecast forecastItemList={forecastItemList}/>)
