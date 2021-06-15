f0278065eb0b6082d8c381760e2e505f72a0cc05
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-15 06:50:07 -0400

    Pages creation

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const WelcomePage = props => {
+    return (
+        <div>
+            Welcome
+        </div>
+    )
+}
+
+WelcomePage.propTypes = {
+
+}
+
+export default WelcomePage
