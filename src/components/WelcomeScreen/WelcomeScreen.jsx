53c90dca2e01e34fb797b4701eb4881ccbfd1cd2
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-20 08:28:45 -0400

    Interaction of useState and useEffect

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,8 +1,9 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 
 const WelcomeScreen = ({children}) => {
     const myRefDiv = useRef(null) // Valor inicial
+    const [vanta, setVanta] = useState(0) // Vanta va a ser inicializado en "0"
     
     // En la primera renderización "MyRefDiv.current" es igual
     // a "nulo", el valor inicial
@@ -10,6 +11,15 @@ const WelcomeScreen = ({children}) => {
 
     useEffect(() => {
         console.log("myRefDiv.current (en useEffect)", myRefDiv.current)
+
+        // Sólo pasa una vez por dentro del if
+        // vanta === 0, es igual a "vanta == false"
+        // ... aún más corto, es igual a "!vanta"
+        if(!vanta){
+            // SOLO PASA UNA VEZ
+            setVanta(1) // Vanta = 1
+            console.log("Establezco un valor diferente a 0")
+        }
     })
 
     return (
