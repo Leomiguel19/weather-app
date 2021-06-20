1dc43f1c6b2899522ca067909f5cabf30fd5fbb2
Leomiguel19 <leomiguel1907@gmail.com>
2021-06-19 21:46:26 -0400

    Creation of AppFrame component

---
+++ src/components/AppFrame/AppFrame.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AppFrame = props => {
+    return (
+        <div>
+            ApppFrame
+        </div>
+    )
+}
+
+AppFrame.propTypes = {
+
+}
+
+export default AppFrame
