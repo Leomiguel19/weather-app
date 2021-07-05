3cb45213c41ad026c36049d70253c138a5feba36
Leomiguel19 <leomiguel1907@gmail.com>
2021-07-04 23:19:27 -0400

    Icon changes

--- src/components/Forecast/Forecast.stories.jsx
+++ src/components/Forecast/Forecast.stories.jsx
@@ -9,7 +9,7 @@ export default {
 const forecastItemList = [
     { hour: 18, state:"sunny", temperature:17, weekDay:"Jueves" },
     { hour: 12, state:"cloud", temperature:18, weekDay:"Viernes"},
-    { hour: 6, state:"fog", temperature:18, weekDay:"Viernes"},
+    { hour: 6, state:"cloudy", temperature:18, weekDay:"Viernes"},
     { hour: 14, state:"cloudy", temperature:19, weekDay:"Viernes"},
     { hour: 15, state:"rain", temperature:17, weekDay:"Sábado"},
     { hour: 9, state:"rain", temperature:17, weekDay:"Sábado"},
